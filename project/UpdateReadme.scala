b6a3f5d8120c94574847bc075f7adefcfaddb5fc
kenji yoshida <6b656e6a69@gmail.com>
2026-06-28 10:05:31 +0900

    Fix UpdateReadme.scala

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -6,17 +6,16 @@ import scala.sys.process.Process
 object UpdateReadme {
   val scalapropsDerivingName = "scalaprops-deriving"
 
-  val updateReadmeTask = { state: State =>
+  val updateReadmeTask: State => State = { state =>
     val extracted = Project.extract(state)
-    val v = extracted get version
-    val org = extracted get organization
-    val snapshot = extracted get isSnapshot
+    val v = extracted.get(version)
+    val org = extracted.get(organization)
+    val snapshot = extracted.get(isSnapshot)
     val modules = Seq(scalapropsDerivingName)
     val readme = "README.md"
     val readmeFile = file(readme)
-    val newReadme = Predef
-      .augmentString(IO.read(readmeFile))
-      .lines
+    val newReadme = IO
+      .readLines(readmeFile)
       .map { line =>
         if (line.startsWith("libraryDependencies") && line.contains(" %% ") && !snapshot) {
           val i = modules.map("\"" + _ + "\"").indexWhere(line.contains)
@@ -27,7 +26,7 @@ object UpdateReadme {
       }
       .mkString("", "\n", "\n")
     IO.write(readmeFile, newReadme)
-    val git = new Git(extracted get baseDirectory)
+    val git = new Git(extracted.get(baseDirectory))
     git.add(readme) ! state.log
     git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
     Process("git diff HEAD^") ! state.log
